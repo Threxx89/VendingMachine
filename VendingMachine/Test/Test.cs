b97355d7056095774d55e350516e17aaed46da81
darrel.frade <darrel.frade@tap-ic.co.jp>
2025-03-05 13:06:46 +0900

    reference dependency test

---
+++ VendingMachine/Test/Test.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VendingMachine.Test
+{
+    internal class Test
+    {
+        public string car = "car";
+        public Test2 Test2;
+
+        public string GetCar()
+        {
+            return car;
+        }
+
+        public string GetWheel()
+        {
+            return Test2.GetWheel();
+        }
+
+        public Test()
+        {
+            
+        }
+    }
+}
---
+++ VendingMachine/Test/Test2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VendingMachine.Test
+{
+    internal class Test2
+    {
+        public string wheel = "wheel";
+        public Test Test;
+
+        public string GetWheel()
+        {
+            return wheel;
+        }
+
+        public string GetCar()
+        {
+            return Test.GetCar();
+        }
+
+        public Test2()
+        {
+            
+        }
+    }
+}
